3e57e3d05d1afa7c838de1575a9e91d9ff729bca
caio <caiosantos.costa02@gmail.com>
2024-02-01 00:56:06 -0300

    update

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent implements OnInit {
+  photoCover: string = '';
+  cardTitle: string = 'Update your game list now';
+  cardDescription: string = 'streamings announces new games that are starting to gain conversation on social media around the world';
 
-  constructor() { }
+  
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
